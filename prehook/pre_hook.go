17d31f805b4b936301a6d6bac9fdf7a15e2fcd60
allen.wang <allen.wq@alipay.com>
2018-01-29 22:58:12 +0800

    add systemd and init for richcontainer
    Signed-off-by: allen.wang <allen.wq@alipay.com>

--- prehook/pre_hook.go
+++ prehook/pre_hook.go
@@ -4,7 +4,9 @@ package prehook
 
 import (
 	"github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/urfave/cli"
 	"sync"
+	"path/filepath"
 )
 
 type HookOptions struct {
@@ -46,4 +48,24 @@ func PreHook(opt *HookOptions, spec *specs.Spec) error {
 	}
 
 	return nil
+}
+
+func CreateHookOptions(context* cli.Context, spec *specs.Spec) (*HookOptions,error) {
+	rootfsPath := ""
+
+	if filepath.IsAbs(spec.Root.Path){
+		rootfsPath = spec.Root.Path
+	}else {
+		p,err := filepath.Abs(filepath.Join(context.String("bundle"), spec.Root.Path))
+		if err != nil {
+			return nil, err
+		}
+
+		rootfsPath = p
+	}
+
+	return &HookOptions{
+		RootfsDir: rootfsPath,
+		ID: context.Args().First(),
+	},nil
 }
