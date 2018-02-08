109b3591e47921f0e8916ba7654c45ec0e99a10b
allen.wang <allen.wq@alipay.com>
2018-02-08 17:25:29 +0800

    if open log file falied, write to /dev/null

--- utils/log.go
+++ utils/log.go
@@ -4,6 +4,8 @@ import (
 	"os"
 
 	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"github.com/gogo/protobuf/io"
 )
 
 const defaultLogPath = "/run/runc.log"
@@ -17,13 +19,18 @@ func GetLogger() *logrus.Logger {
 }
 
 func NewLogger() *logrus.Logger {
-	f, err := os.OpenFile(defaultLogPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0x644)
-	if err != nil {
-		panic(err)
+	var(
+		w io.Writer = ioutil.Discard
+	)
+
+	f, err := os.OpenFile(defaultLogPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND | os.O_SYNC, 0x644)
+	if err == nil {
+		//if open file ok, set writer to file, else to /dev/null
+		w = f
 	}
 
 	return &logrus.Logger{
-		Out:       f,
+		Out:       w,
 		Formatter: new(logrus.TextFormatter),
 		Hooks:     make(logrus.LevelHooks),
 		Level:     logrus.InfoLevel,
