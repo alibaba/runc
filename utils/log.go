41a6b936ad8caf63df817d0d4dc7bfaf121404be
allen.wang <allen.wq@alipay.com>
2018-02-08 17:28:27 +0800

    format source file in go fmt standrd
    Signed-off-by: allen.wang <allen.wq@alipay.com>

--- utils/log.go
+++ utils/log.go
@@ -1,11 +1,11 @@
 package utils
 
 import (
+	"io"
+	"io/ioutil"
 	"os"
 
 	"github.com/sirupsen/logrus"
-	"io/ioutil"
-	"github.com/gogo/protobuf/io"
 )
 
 const defaultLogPath = "/run/runc.log"
@@ -19,11 +19,11 @@ func GetLogger() *logrus.Logger {
 }
 
 func NewLogger() *logrus.Logger {
-	var(
+	var (
 		w io.Writer = ioutil.Discard
 	)
 
-	f, err := os.OpenFile(defaultLogPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND | os.O_SYNC, 0x644)
+	f, err := os.OpenFile(defaultLogPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND|os.O_SYNC, 0x644)
 	if err == nil {
 		//if open file ok, set writer to file, else to /dev/null
 		w = f
